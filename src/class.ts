143f1b1961cd8f68766bbb2bf80654bcb30f68db
zhucaiyun <zhucaiyun1@xdf.cn>
2022-04-21 18:29:48 +0800

    feat:class and interface

--- src/class.ts
+++ src/class.ts
@@ -2,7 +2,7 @@
  * @Author       : zhucaiyun1@xdf.cn
  * @Date         : 2022-04-19 18:00:42
  * @LastEditors  : zhucaiyun1@xdf.cn
- * @LastEditTime : 2022-04-20 17:20:18
+ * @LastEditTime : 2022-04-21 18:29:12
  * @Description  : 类的继承和成员修饰符
  */
 class Dog { 
@@ -38,3 +38,114 @@ class Husky extends Dog {
 * private
 * protocol
 */
+
+
+/*
+* 抽象类：抽取公共的属性方法；实现多态
+* 多态
+* 
+*/
+abstract class Animal { 
+  constructor(name: string) { 
+    this.name = name
+  }
+  name: string
+  abstract legs: number
+  abstract getVoice(): void
+  getColor(voice:string): void { 
+    // 非abstract的必须有函数实体
+    console.log(voice)
+  }
+}
+
+// Cat = new Animal() // 无法创建抽象类的实例。
+class Cat extends Animal { 
+  constructor(public name:string) { 
+    super(name)
+  }
+  legs: number = 4
+  getVoice(): void { // 多态
+    console.log('喵喵')
+  }
+  getEmotion(): void { 
+    console.log('wild')
+  }
+}
+let tom: Animal
+tom  = new Cat('猫')
+tom.getColor('orange')
+// tom.getEmotion() // 类型“Animal”上不存在属性“getEmotion”。 let tom = new Cat('猫')
+
+class Dahuang extends Animal { 
+  legs: number = 4
+  getVoice(): void {  // 多态
+    console.info('汪汪')
+  }
+}
+let dahuang = new Dahuang('狗')
+let animals: Animal[] = [tom, dahuang]
+
+animals.forEach(anItem => {
+  anItem.getVoice()
+});
+
+// this链 方法的链式调用
+class WorkFlow { 
+  step1() { 
+    console.log('1')
+    return this
+  }
+  step2() { 
+    console.log('2')
+    return this
+  }
+  step3() { 
+    console.log('3')
+  }
+}
+// new WorkFlow().step1().step3().step2() //类型“void”上不存在属性“step2”。 
+new WorkFlow().step1().step2().step3()
+// this多态，可以指向子类或者父类
+class Myflow extends WorkFlow { 
+  next() { 
+    return this
+  }
+}
+console.log(new Myflow().next()) // Myflow
+console.log(new Myflow().step1()) // Myflow
+console.log(new Myflow().next().step1()) // Myflow  // 我理解这里都是子类的this吧
+
+// 类和接口
+interface animals { 
+  legs: number;
+  getVoice?():void
+}
+
+class Ani implements animals { 
+  legs = 4
+  getVoice(): void {
+    console.log('voice')
+  }
+}
+
+interface bio extends animals { 
+  type?: string;
+  getTypes(): string;
+}
+
+class Bio implements bio { 
+  getTypes() { 
+    return 'fly'
+  }
+  legs = 2
+}
+
+class Sea { 
+  size: string = 'big'
+}
+
+interface sea extends Sea { }
+
+class allSea implements sea { 
+  size = 'small'
+}
