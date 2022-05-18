90d8c096d7cfb5cea1d0db5e2c3b2203c2de8c6d
zhucaiyun <zhucaiyun1@xdf.cn>
2022-05-18 12:13:53 +0800

    feat:基础篇

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,5 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+// fork-ts-checker-webpack-plugin 类型检查
+// awesome-typescript-loader 和ts-loader同类只是内部集成了类型检查 并可以在独立进程中进行的
+// babel
 module.exports = {
     entry: './src/index.ts',
     output: {
@@ -13,7 +15,10 @@ module.exports = {
             {
                 test: /\.tsx?$/i,
                 use: [{
-                    loader: 'ts-loader'
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true // 快速构建不检查类型
+                    }
                 }],
                 exclude: /node_modules/
             }
